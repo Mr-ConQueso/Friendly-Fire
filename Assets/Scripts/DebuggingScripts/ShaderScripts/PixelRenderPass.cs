96dbfc6c98a58075525019512cb25cb40d7c72ae
Mr. ConQueso <78820389+Mr-ConQueso@users.noreply.github.com>
2024-10-23 12:48:38 +0200

    Removed NuGet & PostHog

--- Assets/Scripts/DebuggingScripts/ShaderScripts/PixelRenderPass.cs
+++ Assets/Scripts/DebuggingScripts/ShaderScripts/PixelRenderPass.cs
@@ -35,31 +35,9 @@ public class PixelRenderPass : ScriptableRenderPass
     }
 
     public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-    {
-        if (renderingData.cameraData.cameraType == CameraType.SceneView && !renderingData.cameraData.postProcessEnabled)
-        {
-            return;
-        }
+         {
 
-        if (_pixelSettings == null || !_pixelSettings.IsActive())
-        {
-            return;
-        }
-
-        CommandBuffer cmd = CommandBufferPool.Get("Pixelate Post Process");
-
-        // Set pixel size property based on the strength from _pixelSettings
-        int pixelSize = Mathf.Max(1, Mathf.CeilToInt(_pixelSettings.pixelSize.value));
-        _material.SetInteger(PixelSize, pixelSize);
-
-        // Execute the pixelation effect
-        cmd.Blit(_source, _pixelTextID, _material, 0);  // First pass: apply pixelation to temporary texture
-        cmd.Blit(_pixelTextID, _source);                // Second pass: render back to the source
-
-        context.ExecuteCommandBuffer(cmd);
-        cmd.Clear();
-        CommandBufferPool.Release(cmd);
-    }
+         }
 
     public override void FrameCleanup(CommandBuffer cmd)
     {
